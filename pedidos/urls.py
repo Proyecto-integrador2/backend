67afd9f18fee0b1f8472377c8a6f5aeb4133acb0
DianaCadena <diana.marcela.cadena@correounivalle.edu.co>
2024-09-16 15:31:12 -0500

    update reports views.py

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import *
 
 router = DefaultRouter()
 router.register(r'mesas', views.MesaViewSet)
@@ -12,4 +13,7 @@ router.register(r'history', views.OrderHistoryViewSet, basename='order-history')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/reports/', OrderReportView.as_view(), name='order-reports'),
+    path('api/reports/category/', ProductCategoryReportView.as_view(), name='product-category-reports'),
+    path('api/reports/product/', ProductNameReportView.as_view(), name='product-name-reports'),
 ]
